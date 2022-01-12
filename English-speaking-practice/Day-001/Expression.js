63cd4d4cfda6030c719569fdd52b18d7896b8814
Maidul Muqtadir <86922545+MMaidul@users.noreply.github.com>
2022-01-12 11:38:30 +0600

    English practice

---
+++ English-speaking-practice/Day-001/Expression.js
@@ -0,0 +1,13 @@
+/*------------------------------------
+I shouldn't wonder if(used brithish English) - আমি অবাক হবো নাহ্
+I shouldn't wonder if she doesn't accept your love proposallShe has a boyfriend.
+
+There's nothing to it.(it's so simple / easy) -এটা কোন ব্যাপারই নাহ্
+Anyone can use a computer .There's nothig to it.
+Everybody can browse facebook.There's nothing to it.
+Let me show how to fix it.There's nothing to it.
+
+start over-নতুনভাবে শুরু করা
+If you make mistake.Then you have to start over.
+I'm getting a little bit too old to start over.
+-------------------------------------*/
