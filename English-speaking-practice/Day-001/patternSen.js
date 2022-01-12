7901d46ffbb0d1d66c9fb68fdd7f03d8405d7571
Maidul Muqtadir <86922545+MMaidul@users.noreply.github.com>
2022-01-12 11:53:26 +0600

    Add files via upload

---
+++ English-speaking-practice/Day-001/patternSen.js
@@ -0,0 +1,11 @@
+/*----------------------------------
+(i)Out of order/doesn't work (electronic device/machine)
+
+Explanation:You use this expression to say that a particular device has become unusable and needs to be repaired. 
+
+
+Can you tell the time, please? My watch is out of order.
+I gotta buy a new calculator.It doesn't work.
+The elevator was out of order and we had to climb to the 9th editation
+
+-----------------------------------*/
